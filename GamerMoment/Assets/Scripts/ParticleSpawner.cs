d92a520a66ee59947f69a0a8fdf1cb941340d892
hellgo <efeponte@gmail.com>
2022-02-05 19:46:27 +0100

    barticle

--- GamerMoment/Assets/Scripts/ParticleSpawner.cs
+++ GamerMoment/Assets/Scripts/ParticleSpawner.cs
@@ -11,6 +11,7 @@ public class ParticleSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        emitter = GetComponent<ParticleSystem>();
         emitter.Stop();
     }
 
